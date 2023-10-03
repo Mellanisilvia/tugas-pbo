3ef69f6e8f24dc8b21fd0c61449f97267c465c97
Mellanisilvia <124517725+Mellanisilvia@users.noreply.github.com>
2023-10-03 17:30:44 +0700

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,39 +1,67 @@
 /**
- * kelas Circle menggambarkan sebuah lingkaran dengan Jari Jari dan warna.
+ * Kelas Circle menggambarkan Lingkaran dengan radius dan warna
  */
-public class Circle {
-    // Variabel instan privat 
-    private double Jarijari;
-    private String Warna;
- 
-    // Konstruktor (di-overload)
-    /**Membuat sebuah instance lingkaran dengan radius dan warna default*/
-    public Circle() {                   // konstruktor pertama  (konstruktor default)
-       Jarijari = 1.0;
-       Warna = "Merah";
-    }
-    /** Membuat sebuah instance lingkaran dnegan radius tertentu dan warna default*/
-    public Circle(double r) {           // konstruktor kedua
-       Jarijari = r;
-       Warna = "Merah";
-    }
-    /** Membuat sebua instance lingkaran dengan radius dan warna tertentu */
-    public Circle(double r, String c) { // konstruktor ketiga
-       Jarijari = r;
-       Warna = c;
-    }
- 
-    // Metode public
-    /** mengembalikan nilai dan jari jari */
-    public double getJarijari() {  // getter untuk Jari Jari 
-       return Jarijari;
-    }
-    /** Mengembalikan nilai warna  */
-    public String getWarna() {   // getter untuk warna
-       return Warna;
-    }
-    /** Mengembalikan luas dan lningkaran  */
-    public double getLuas() {
-       return Jarijari * Jarijari * Math.PI;
-    }
+
+ public class Circle{   //simpan sebagai "Circle.java"
+   //Konstanta publik
+   public static final double DEFAULT_RADIUS = 1.0;
+   public static final String DEFAULT_WARNA  = "Merah";
+
+   //Variabel instance pribadi
+   private double radius;
+   private String warna;
+
+   //Konstruktor (oveloaded)
+   // Membuat objek  circle dengan radius dan waran 
+   public Circle(){                 //konstruktor pertama 
+      this.radius = DEFAULT_RADIUS;
+      this.warna  = DEFAULT_WARNA;
+   }
+
+   //Membuat objek Circle dengan radius tertentu dan warna default
+   public Circle(double radius){ //konstruktor kedua
+      this.radius = radius;
+      this.warna  = DEFAULT_WARNA; 
+   }
+
+   //Membuat objek circle dengan radius dan warna tertentu
+   public Circle(double radius, String warna ){ //konstruktor ketiga
+      this.radius = radius;
+      this.warna  = warna;
+   }
+
+   //Mengembalikan nilai rdius metode setter untuk variabel radius
+   public double getRadius(){
+      return this.radius;
+   }
+
+   //Mengatur nilai radius metode setter untuk variabel radius 
+   public void setRadius(double radius){
+      this.radius = radius;
+   } 
+
+   //Mengembalikan warna metode getter untuk variabel color
+   public String getWarna(){
+      return this.warna;
+   }
+
+   //Mengatur warna metode setter untuk variabel warna
+   public void setWarna(String warna){
+      this.warna = warna;
+   }
+
+   //Mengembalikan string yang menjelaskan objek Circle
+   public String toString(){
+      return "Lingkaran [radius =" + radius + ", warna = " + warna + "]";
+   }
+
+   //mengembalikan luas dari circle
+   public double getLuas(){
+      return radius * radius * Math.PI;
+   }
+
+   //Mengambalikan keliling dari circle 
+   public double getKeliling(){
+      return 2.0 * radius * Math.PI;
+   }
  }
